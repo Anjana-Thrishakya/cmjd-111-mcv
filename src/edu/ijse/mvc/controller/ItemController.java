1716b28378da640e8bb5acd9d0e8b6880e338dda
Anjana Thrishakya <anjanathrishakya@gmail.com>
2025-06-08 12:42:50 +0530

    ItemModel save Item with total flow

--- src/edu/ijse/mvc/controller/ItemController.java
+++ src/edu/ijse/mvc/controller/ItemController.java
@@ -5,6 +5,7 @@
 package edu.ijse.mvc.controller;
 
 import edu.ijse.mvc.dto.ItemDto;
+import edu.ijse.mvc.model.ItemModel;
 
 /**
  *
@@ -12,8 +13,10 @@ import edu.ijse.mvc.dto.ItemDto;
  */
 public class ItemController {
     
+    private ItemModel itemModel = new ItemModel();
+    
     public String saveItem(ItemDto itemDto) throws Exception{
-        return "Success";
+        return itemModel.saveItem(itemDto);
     }
     
 }
